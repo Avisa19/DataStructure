c6aa550a2844236ea1ed2ce074e8b4e8539926a1
Avisa19 <avisa_poshtkouhi@yahoo.com>
2019-04-24 19:15:22 +1000

    LowerPopulation.

---
+++ DataStructure/PopulationStruct.swift
@@ -0,0 +1,13 @@
+//
+//  PopulationStruct.swift
+//  DataStructure
+//
+//  Created by Avisa on 24/4/19.
+//  Copyright © 2019 Avisa Poshtkouhi. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+
+
